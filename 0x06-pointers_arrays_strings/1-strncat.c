8159b037e48a6376152566afdeb3ee90d27fdd68
Elom Laurent <laziagbenyo@gmail.com>
2023-03-13 01:55:04 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - Concatenate two strings using inputted number of bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: the number of byte
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i;
+	int j;
+
+	while (dest[i++])
+	{
+		j++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[j++] = src[i];
+	}
+	return (dest);
+
+}
