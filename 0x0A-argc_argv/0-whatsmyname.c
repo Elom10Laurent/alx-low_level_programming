38b3f5dd86a16924a83a4a81bee028cc2d72be62
Elom10Laurent <laziagbenyo@gmail.com>
2023-06-11 12:58:36 -0700

    prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - prints the program name;
- * @argc:input
- * @argv:input 
- * Return: O 'Success'
+ * @argc: input
+ * @argv: input
+ * Return: 0 'Success'
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
+
 	printf("%s\n", *argv);
 	return (0);
 }
