b092a009cade21b0c99a1494a86e5d780b829f66
Elom10Laurent <laziagbenyo@gmail.com>
2023-08-12 06:35:49 -0700

    late

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main -  print char with printf function 
+ *
+ * return: Always 0 (Success)
+ */
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
