6b604b0a32c8f770ea1e94ff7e8c16640f0e53f9
Elom10Laurent <laziagbenyo@gmail.com>
2023-08-12 07:09:08 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /*
  * main - print char with puts function
  *
+ * void
  * Return:  Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
