2bbfdfd1e1367478373b8631a8fc33c6389d7ce0
Elom10Laurent <laziagbenyo@gmail.com>
2023-08-13 01:29:34 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - Entry point 
+ *main - A program that print line with printf function
  *
  *void
- *return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
