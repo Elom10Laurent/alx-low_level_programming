d48deb7b87fa1d7880defbb4cb0e5ff3b8f47057
Elom10Laurent <laziagbenyo@gmail.com>
2023-08-16 02:12:30 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Void parameter
+ * Return: Always void
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0;
+	char y;
+
+	while ( x < 10)
+	{
+		
+		for (y = 'a'; y <='z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+		x++;
+	}
+}
