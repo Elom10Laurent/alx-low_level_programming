937fd1113a533da29949400049dad94b38a96dc0
Elom Laurent <laziagbenyo@gmail.com>
2025-04-19 13:04:14 +0000

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') /* Base case: end of string */
-    {
-        _putchar('\n'); /* Print newline character */
-        return;
-    }
-    _putchar(*s);       /* Print the current character */
-    _puts_recursion(s + 1); /* Recursive call with next character */
+	if (*s == '\0') 
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
