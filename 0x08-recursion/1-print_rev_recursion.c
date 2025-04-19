b42b798fea8a7f9249e64f1dc3b1836ff8980d6c
Elom Laurent <laziagbenyo@gmail.com>
2025-04-19 13:57:16 +0000

    Write a function that prints a string in reverse.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,10 +8,12 @@
  * null terminator is reached. Then, as the function calls unwind,
  * it prints each character in reverse order.
  */
-void _print_rev_recursion(char *s) {
-    if (*s == '\0') {
-        return;
-    }
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
